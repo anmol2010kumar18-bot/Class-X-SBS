bd7352124c5c90c8d5dd2d2d27832ad424c00a87
Anmol Kumar <anmol2010kumar18@gmail.com>
2026-06-06 08:29:26 +0530

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,12 @@
+window._APP_CONFIG = {
+  firebase: {
+    apiKey:            "YOUR_FIREBASE_API_KEY",
+    authDomain:        "YOUR_PROJECT.firebaseapp.com",
+    databaseURL:       "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+    projectId:         "YOUR_PROJECT_ID",
+    storageBucket:     "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId:             "YOUR_APP_ID"
+  },
+  groqKey: "YOUR_GROQ_API_KEY"
+};
