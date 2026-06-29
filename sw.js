3bf9c3ce94df515746beeee4e2bb7bd46eb77c67
Anmol Kumar <anmol2010kumar18@gmail.com>
2026-06-29 18:17:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — Service Worker for USCD DAV Class X SBS
-const CACHE = 'davx-v5';  // bump this on every new deployment
+const CACHE = 'davx-v6';  // bump this on every new deployment
 const SHELL = ['./'];
 
 self.addEventListener('install', e => {
